527c11efaaba7bf3769c0527fe5868d0fd1b7197
Al Madireddy <al.madireddy@gmail.com>
2021-05-24 22:03:46 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [events, setEvents] = useState(null);
+
+  useEffect(() => {
+
+    const getEvents = async () => {
+      const req = await fetch("https://funzippy.com/event/search?csrftkn=kmdDO", {
+        method: "post",
+        body: JSON.stringify({
+          "pageNo":1,
+          "resultsPerPage":20,
+          "radius":50,
+          "dateRange":"",
+          "name":"",
+          "city":"",
+          "category1":""
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      const data = await req.json();
+
+      setEvents(data);
+    }
+
+    getEvents();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +45,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hey there
         </a>
       </header>
     </div>
